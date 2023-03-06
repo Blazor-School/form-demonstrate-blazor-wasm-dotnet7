e07f0306fb4e729130ae75c4dd1cc9556f74f009
Red Wei <huy.tran@blazorschool.com>
2023-03-06 22:28:22 +0700

    Update example.

--- FormDemonstrate/FormModels/BasicBlazorFormModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace FormDemonstration.FormModels;
-
-public class BasicBlazorFormModel
-{
-    public string ExampleString { get; set; } = "Blazor School";
-}
---
+++ FormDemonstrate/FormModels/SimpleBlazorFormModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormDemonstration.FormModels;
+
+public class SimpleBlazorFormModel
+{
+    public string InputText { get; set; } = "Blazor School";
+}
